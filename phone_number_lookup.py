8228d3a64a3b0f41fb9a3b1a3cf191da0b40b4ae
arindambiswas91 <39226872+arindambiswas91@users.noreply.github.com>
2018-05-13 04:30:03 +0530

    Add files via upload

---
+++ phone_number_lookup.py
@@ -0,0 +1,18 @@
+import requests
+from lxml import html
+import sys
+
+def search_phone(number):
+	r=requests.get("https://cellrevealer.com/Results/"+number+"/")
+	tree=html.fromstring(r.content)
+	name = tree.xpath("//div[@class='small-12 large-7 medium-7 columns']/table[@class='reverse-phone-results-table']/tbody/tr[3]/td[@class='value']/text()")
+	location= tree.xpath("//div[@class='small-12 large-7 medium-7 columns']/table[@class='reverse-phone-results-table']/tbody/tr[5]/td[@class='value']/text()")
+	#rint(name[0])
+	#print(location[0])
+	return name,location
+
+
+if __name__ == '__main__':
+
+	#number=sys.argv[1]
+	search_phone('4082307112')
